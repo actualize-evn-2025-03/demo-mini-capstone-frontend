5b20744967e890133368411be06e9aaf1ab5e41b
Leon Shimizu <lmshimizu@gmail.com>
2025-07-03 11:01:32 +1000

    Implement Products New - incomplete because needs authentication (needs to be admin)

--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { ProductsIndex } from "./ProductsIndex";
+import { ProductsNew } from "./ProductsNew";
 
 export function ProductsPage() {
   const [products, setProducts] = useState([]);
@@ -13,10 +14,20 @@ export function ProductsPage() {
     });
   };
 
+  const handleCreate = (params, successCallback) => {
+    console.log("handleCreate");
+    axios.post("http://localhost:3000/products.json", params)
+      .then((response) => {
+        setProducts([...products, response.data]);
+        successCallback();
+      })
+  }
+
   useEffect(handleIndex, []);
 
   return (
     <main>
+      <ProductsNew onCreate={handleCreate} />
       <ProductsIndex products={products} />
     </main>
   );
